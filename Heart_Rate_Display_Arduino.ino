a391a3a873dc121009e344f18a29303773d2e33a
Steven Keyes <stevenrkeyes@gmail.com>
2022-05-25 17:29:38 -0700

    Used simple filter, and changed LED to just blip

--- Heart_Rate_Display_Arduino.ino
+++ Heart_Rate_Display_Arduino.ino
@@ -1,31 +1,5 @@
-/******************************************************************************
-Heart_Rate_Display.ino
-Demo Program for AD8232 Heart Rate sensor.
-Casey Kuhns @ SparkFun Electronics
-6/27/2014
-https://github.com/sparkfun/AD8232_Heart_Rate_Monitor
-
-The AD8232 Heart Rate sensor is a low cost EKG/ECG sensor.  This example shows
-how to create an ECG with real time display.  The display is using Processing.
-This sketch is based heavily on the Graphing Tutorial provided in the Arduino
-IDE. http://www.arduino.cc/en/Tutorial/Graph
-
-Resources:
-This program requires a Processing sketch to view the data in real time.
-
-Development environment specifics:
-	IDE: Arduino 1.0.5
-	Hardware Platform: Arduino Pro 3.3V/8MHz
-	AD8232 Heart Monitor Version: 1.0
-
-This code is beerware. If you see me (or any other SparkFun employee) at the
-local pub, and you've found our code helpful, please buy us a round!
-
-Distributed as-is; no warranty is given.
-******************************************************************************/
-
+// CircularBuffer library from AgileWare, accessible from the Arduino library manager
 #include <CircularBuffer.h>
-#include <SoftPWM.h>
 
 
 #define LED_PIN LED_BUILTIN
@@ -34,37 +8,12 @@ Distributed as-is; no warranty is given.
 
 long beat_detected_time_ms;
 
-void led_heartbeat(uint8_t led_pin, long start_time_ms) {
-  int fade_value;
-  long current_time_ms = millis();
-  long elapsed_time_ms = current_time_ms - start_time_ms;
-
-  if (elapsed_time_ms < 100) {
-    fade_value = (int)elapsed_time_ms;
-  } else if (elapsed_time_ms < 200) {
-    fade_value = 100 - (elapsed_time_ms - 100);
-  } else if (elapsed_time_ms < 250) {
-    fade_value = 0;
-  } else if (elapsed_time_ms < 290) {
-    fade_value = elapsed_time_ms - 250;
-  } else if (elapsed_time_ms < 330) {
-    fade_value = 40 - (elapsed_time_ms - 290);
-  } else {
-    fade_value = 0;
-  }
-  
-  SoftPWMSet(led_pin, fade_value);
-}
-
 void setup() {
   // initialize the serial communication:
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   pinMode(LED_PIN, OUTPUT);
-  SoftPWMBegin();
-  SoftPWMSetFadeTime(LED_PIN, 0, 0);
-  SoftPWMSet(LED_PIN, 0);
-
+  
   beat_detected_time_ms = 0;
 }
 
@@ -95,10 +44,13 @@ void loop() {
      long candidate_beat_detected_time_ms = millis();
      if (candidate_beat_detected_time_ms - beat_detected_time_ms > COOLDOWN_MS) {
        beat_detected_time_ms = candidate_beat_detected_time_ms;
+       digitalWrite(LED_PIN, HIGH);
+     } else {
+       digitalWrite(LED_PIN, LOW);
      }
+  } else {
+    digitalWrite(LED_PIN, LOW);
   }
-  
-  led_heartbeat(LED_PIN, beat_detected_time_ms);
 
   Serial.println(filtered_measurement);
   
