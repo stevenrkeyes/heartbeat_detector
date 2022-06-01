5b674bb198e8828c093fd30536f2c3ca3f46a7cb
Steven Keyes <stevenrkeyes@gmail.com>
2022-06-01 00:33:23 -0700

    Added electrode connection detection and improved some variable names

--- Heart_Rate_Display_Arduino.ino
+++ Heart_Rate_Display_Arduino.ino
@@ -3,18 +3,24 @@
 
 
 #define LED_PIN LED_BUILTIN
+#define ECG_INPUT_PIN A0
+#define LEADS_OFF_P_PIN A3
 
-#define COOLDOWN_MS 500
+// Only identify r-peaks in the ECG signal if they are spaced farther apart than the following cooldown, to avoid false detections
+#define R_PEAK_COOLDOWN_MS 500
+long beat_detected_system_time_ms = 0;
 
-long beat_detected_time_ms;
+// The leads off signal is a little noisy, so assume the electrodes are disconnected if a leads-off has been detected within the following delay
+#define LEADS_OFF_COOLDOWN_MS 200
+long last_leads_off_system_time_ms = 0;
 
 void setup() {
-  // initialize the serial communication:
+  // Initialize the serial communication:
   Serial.begin(115200);
 
   pinMode(LED_PIN, OUTPUT);
-  
-  beat_detected_time_ms = 0;
+  pinMode(ECG_INPUT_PIN, INPUT);
+  pinMode(LEADS_OFF_P_PIN, INPUT);
 }
 
 float filtered_measurement = 0;
@@ -24,13 +30,18 @@ float filtered_measurement_low = 0;
 CircularBuffer<float, 200> past_samples;
 
 void loop() {
-
-  int raw_measurement = analogRead(A0);
+  int raw_measurement = analogRead(ECG_INPUT_PIN);
   
   // Do a little bit of digital high- and low- pass filtering
   filtered_measurement_high = 0.8 * filtered_measurement_high + 0.2 * raw_measurement;
   filtered_measurement_low = 0.99 * filtered_measurement_low + 0.01 * raw_measurement;
   filtered_measurement = filtered_measurement_high - filtered_measurement_low;
+
+  // The electrodes (at least the positive one) are disconnected from a person (leads off) if the pin is high
+  bool leads_off = digitalRead(LEADS_OFF_P_PIN);
+  if (leads_off) {
+    last_leads_off_system_time_ms = millis();
+  }
 
   past_samples.push(filtered_measurement);
   float max_sample = 0;
@@ -40,20 +51,26 @@ void loop() {
     }
   }
 
-  if (filtered_measurement > 0.9 * max_sample) {
-     long candidate_beat_detected_time_ms = millis();
-     if (candidate_beat_detected_time_ms - beat_detected_time_ms > COOLDOWN_MS) {
-       beat_detected_time_ms = candidate_beat_detected_time_ms;
-       digitalWrite(LED_PIN, HIGH);
-     } else {
-       digitalWrite(LED_PIN, LOW);
-     }
+  long candidate_beat_detected_system_time_ms = millis();
+  // Check that we are leads on (including the cooldown of any recent leads off events)
+  if (candidate_beat_detected_system_time_ms - last_leads_off_system_time_ms > LEADS_OFF_COOLDOWN_MS) {
+    // Check that the sample is large enough
+    if (filtered_measurement > 0.9 * max_sample) {
+       if (candidate_beat_detected_system_time_ms - beat_detected_system_time_ms > R_PEAK_COOLDOWN_MS) {
+         beat_detected_system_time_ms = candidate_beat_detected_system_time_ms;
+         digitalWrite(LED_PIN, HIGH);
+       } else {
+         digitalWrite(LED_PIN, LOW);
+       }
+    } else {
+      digitalWrite(LED_PIN, LOW);
+    }
   } else {
     digitalWrite(LED_PIN, LOW);
   }
 
   Serial.println(filtered_measurement);
-  
+
   //Wait for a bit to keep serial data from saturating
   delay(1);
 }
